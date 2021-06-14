01966f142f9dab5d64712bdc7a4e804c52064ab4
Cian Dunne <20091471@mail.wit.ie>
2021-06-15 00:19:15 +0100

    Show movie review excerpts; Refactor Details page

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState} from "react";
 import Chip from "@material-ui/core/Chip";
 import Paper from "@material-ui/core/Paper";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
@@ -8,6 +8,8 @@ import NavigationIcon from "@material-ui/icons/Navigation";
 import Fab from "@material-ui/core/Fab";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import MovieReviews from "../movieReviews";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,62 +30,68 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MovieDetails = ( props) => {
-  const classes = useStyles();
-  const movie = props.movie
+const MovieDetails = ({ movie }) => {  // Don't miss this!
+    const classes = useStyles();
+    const [drawerOpen, setDrawerOpen] = useState(false);
+  
 
-  return (
-    <>
-      <Typography variant="h5" component="h3">
-        Overview
-      </Typography>
+    return (
+        <>
+        <Typography variant="h5" component="h3">
+            Overview
+        </Typography>
 
-      <Typography variant="h6" component="p">
-        {movie.overview}
-      </Typography>
+        <Typography variant="h6" component="p">
+            {movie.overview}
+        </Typography>
 
-      <Paper component="ul" className={classes.root}>
-        <li>
-          <Chip label="Genres" className={classes.chip} color="primary" />
-        </li>
-        {movie.genres.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} className={classes.chip} />
-          </li>
-        ))}
-      </Paper>
-      <Paper component="ul" className={classes.root}>
-        <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
-        <Chip
-          icon={<MonetizationIcon />}
-          label={`${movie.revenue.toLocaleString()}`}
-        />
-        <Chip
-          icon={<StarRate />}
-          label={`${movie.vote_average} (${movie.vote_count}`}
-        />
-        <Chip label={`Released: ${movie.release_date}`} />
-      </Paper>
-      <Paper component="ul" className={classes.root}>
-        <li>
-          <Chip label="Production Countries" className={classes.chip} color="primary" />
-        </li>
-        {movie.production_countries.map((g) => (
-          <li key={g.name}>
-            <Chip label={g.name} className={classes.chip} />
-          </li>
-        ))}
-      </Paper>
+        <Paper component="ul" className={classes.root}>
+            <li>
+            <Chip label="Genres" className={classes.chip} color="primary" />
+            </li>
+            {movie.genres.map((g) => (
+            <li key={g.name}>
+                <Chip label={g.name} className={classes.chip} />
+            </li>
+            ))}
+        </Paper>
+        <Paper component="ul" className={classes.root}>
+            <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
+            <Chip
+            icon={<MonetizationIcon />}
+            label={`${movie.revenue.toLocaleString()}`}
+            />
+            <Chip
+            icon={<StarRate />}
+            label={`${movie.vote_average} (${movie.vote_count}`}
+            />
+            <Chip label={`Released: ${movie.release_date}`} />
+        </Paper>
+        <Paper component="ul" className={classes.root}>
+            <li>
+            <Chip label="Production Countries" className={classes.chip} color="primary" />
+            </li>
+            {movie.production_countries.map((g) => (
+            <li key={g.name}>
+                <Chip label={g.name} className={classes.chip} />
+            </li>
+            ))}
+        </Paper>
 
-      <Fab
+        <Fab
         color="secondary"
         variant="extended"
+        onClick={() =>setDrawerOpen(true)}
         className={classes.fab}
-      >
-        <NavigationIcon />
-        Reviews
-      </Fab>
-      </>
+        >
+            <NavigationIcon />
+            Reviews
+        </Fab>
+        <Drawer anchor="top" open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+            <MovieReviews movie={movie} />
+        </Drawer>
+    </>
   );
 };
+
 export default  MovieDetails ;
