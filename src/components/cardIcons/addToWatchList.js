ebdbf7f4ac8ffff99d273c39646277a8c8e16d03
Cian Dunne <20091471@mail.wit.ie>
2021-06-30 20:23:52 +0100

    Changed watchlist icon font to facilitate upload - changed back again.

--- src/components/cardIcons/addToWatchList.js
+++ src/components/cardIcons/addToWatchList.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const WatchListAddIcon = ({ movie }) => {
     return (
-      <PlaylistAddIcon color="primary" fontSize="medium" />
+      <PlaylistAddIcon color="primary" fontSize="large" />
     );
   };
   
